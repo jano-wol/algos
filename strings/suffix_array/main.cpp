1386f40757a74f8806587220f38b1983c706f976
Andrzej Polak <jjjkamu@gmail.com>
2024-01-26 10:31:47 +0100

    First steps toward suffix array

---
+++ strings/suffix_array/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "suffix_array.h"
+
+int main()
+{
+  std::string str = "banana";
+  auto result = suffixArray(str);
+
+  for (auto a : result) {
+    std::cout << a << " ";
+  }
+  std::cout << "\n";
+}
---
+++ strings/suffix_array/suffix_array.h
@@ -0,0 +1,4 @@
+#include <string>
+#include <vector>
+
+std::vector<int> suffixArray(const std::string& str) { return {}; }
