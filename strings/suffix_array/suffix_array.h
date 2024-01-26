38c262789113a3dee1991938a185c6f26eaf8d84
Andrzej Polak <jjjkamu@gmail.com>
2024-01-26 10:41:37 +0100

    Implement check input

--- strings/suffix_array/suffix_array.h
+++ strings/suffix_array/suffix_array.h
@@ -1,4 +1,30 @@
+#ifndef ALGOS_STRINGS_SUFFIX_ARRAY_INCLUDED
+#define ALGOS_STRINGS_SUFFIX_ARRAY_INCLUDED
+
+#include <iostream>
 #include <string>
 #include <vector>
 
-std::vector<int> suffixArray(const std::string& str) { return {}; }
+class SuffixArray
+{
+public:
+  static std::vector<int> suffixArray(const std::string& str)
+  {
+    std::vector<uint8_t> input(str.begin(), str.end());
+
+    checkInput(input);
+
+    return {};
+  }
+
+private:
+  static void checkInput(const std::vector<uint8_t>& input, std::string context = "SuffixArray")
+  {
+    for (auto i : input) {
+      if (i == 0) {
+        std::cerr << "input has zero character! context=" << context << "\n";
+      }
+    }
+  }
+};
+#endif  // ALGOS_STRINGS_SUFFIX_ARRAY_INCLUDED
