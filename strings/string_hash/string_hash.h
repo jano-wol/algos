baaf657cdcccf7b6e9eb98de285c1d9a46dec836
janowol <janos.wolosz@gmail.com>
2024-01-28 16:32:11 +0100

    Add string hash

---
+++ strings/string_hash/string_hash.h
@@ -0,0 +1,21 @@
+#ifndef ALGOS_STRINGS_STRINGHASH_INCLUDED
+#define ALGOS_STRINGS_STRINGHASH_INCLUDED
+
+#include <string>
+
+class StringHash
+{
+public:
+  static long long stringHash(std::string const& s, long long p = 263, long long m = 1e9 + 9)
+  {
+    long long hashValue = 0;
+    long long pow = 1;
+    for (char c : s) {
+      hashValue = (hashValue + (static_cast<int>(c) + 1) * pow) % m;
+      pow = (pow * p) % m;
+    }
+    return hashValue;
+  }
+};
+
+#endif  // ALGOS_STRINGS_STRINGHASH_INCLUDED
