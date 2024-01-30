008f5fb47119eec95fe55af11253963a6f1d1fcd
Andrzej Polak <jjjkamu@gmail.com>
2024-01-30 10:43:45 +0100

    Add prefixFreq example

--- strings/prefix_function/main.cpp
+++ strings/prefix_function/main.cpp
@@ -86,6 +86,42 @@ size_t compress(const std::string& str)
   return ((n % candidate) == 0) ? candidate : n;
 }
 
+std::vector<size_t> prefixFreqNaive(const std::string& str)
+{
+  if (str.empty()) {
+    return {};
+  }
+  auto n = str.size();
+  std::vector<size_t> ret;
+  for (size_t i = 1; i <= n; ++i) {
+    auto currPrefix = str.substr(0, i);
+    size_t r = 0;
+    for (size_t j = 0; j < n - i + 1; ++j) {
+      auto subString = str.substr(j, i);
+      if (currPrefix == subString) {
+        ++r;
+      }
+    }
+    ret.push_back(r);
+  }
+  return ret;
+}
+
+std::vector<size_t> prefixFreq(const std::string& str)
+{
+  if (str.empty()) {
+    return {};
+  }
+  auto n = str.size();
+  std::vector<size_t> ret(n + 1);
+  auto pi = PrefixFunction::prefixFunction(str);
+  for (int i = 0; i < n; i++) ret[pi[i]]++;
+  for (int i = n - 1; i > 0; i--) ret[pi[i - 1]] += ret[i];
+  for (int i = 0; i <= n; i++) ret[i]++;
+  ret.erase(ret.begin());
+  return ret;
+}
+
 int main()
 {
   std::string str = "aabaaab";
@@ -99,14 +135,26 @@ int main()
   for (auto a : resultNaive) {
     std::cout << a << " ";
   }
-  std::cout << "\n";
+  std::cout << "\n---------\n";
 
   str = "ab";
   std::string text = "aaaabababababababbbbbabbbb";
   std::cout << naiveCountSubstring(str, text) << "\n";
-  std::cout << countSubstring(str, text) << "\n";
+  std::cout << countSubstring(str, text) << "\n---------\n";
 
   str = "aabaaabaaaba";
   std::cout << naiveCompress(str) << "\n";
   std::cout << compress(str) << "\n";
+
+  str = "aabaaabaaaba";
+  resultNaive = prefixFreqNaive(str);
+  result = prefixFreq(str);
+  for (auto a : resultNaive) {
+    std::cout << a << " ";
+  }
+  std::cout << "\n";
+  for (auto a : result) {
+    std::cout << a << " ";
+  }
+  std::cout << "\n---------\n";
 }
