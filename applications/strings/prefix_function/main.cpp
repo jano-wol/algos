67454a3596ff9300e7358fe3c136cf401e58e571
Andrzej Polak <jjjkamu@gmail.com>
2024-02-14 20:23:47 +0100

    Add prefix function

---
+++ applications/strings/prefix_function/main.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "./../../../strings/prefix_function/prefix_function.h"
+
+std::vector<size_t> naivePrefixFunction(const std::string& str)
+{
+  size_t n = str.size();
+  std::vector<size_t> pi(n);
+  for (size_t i = 0; i < n; i++)
+    for (size_t k = 0; k <= i; k++)
+      if (str.substr(0, k) == str.substr(i - k + 1, k))
+        pi[i] = k;
+  return pi;
+}
+
+int main()
+{
+  std::string str = "aabaaab";
+  auto piNaive = naivePrefixFunction(str);
+  auto pi = PrefixFunction::prefixFunction(str);
+
+  for (auto a : piNaive) {
+    std::cout << a << " ";
+  }
+  std::cout << "\n";
+  for (auto a : pi) {
+    std::cout << a << " ";
+  }
+  std::cout << "\n";
+}
