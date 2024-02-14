d92b948358c9619c8f1d412f1774bcd60557a767
Andrzej Polak <jjjkamu@gmail.com>
2024-02-14 21:16:41 +0100

    Add compress example

---
+++ applications/strings/compress/main.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "./../../../strings/z_function/z_function.h"
+
+size_t compressNaive(const std::string& str)
+{
+  if (str.empty()) {
+    return 0;
+  }
+  auto n = str.size();
+  for (size_t i = 1; i <= n; ++i) {
+    if (n % i != 0) {
+      continue;
+    }
+    auto curr = str.substr(0, i);
+    bool compressSuccess = true;
+    for (size_t j = 0; j < n / i; ++j) {
+      auto curr2 = str.substr(j * i, i);
+      if (curr != curr2) {
+        compressSuccess = false;
+        break;
+      }
+    }
+    if (compressSuccess) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+// runtime = O(n), memory = O(n), where n = |str|.
+size_t compressZFunction(const std::string& str)
+{
+  if (str.empty()) {
+    return 0;
+  }
+  auto n = str.size();
+  auto z = ZFunction::zFunction(str);
+  for (size_t i = 1; i < n; ++i) {
+    if (n % i != 0) {
+      continue;
+    }
+    if (i + z[i] == n) {
+      return i;
+    }
+  }
+  return n;
+}
+
+int main()
+{
+  std::string str = "aabaaabaaaba";
+  std::cout << compressNaive(str) << "\n";
+  std::cout << compressZFunction(str) << "\n";
+}
