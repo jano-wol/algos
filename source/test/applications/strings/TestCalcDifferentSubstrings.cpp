8cac100096068eb4e6f6d4a61eb14e7b7a0f1510
Andrzej Polak <jjjkamu@gmail.com>
2024-02-15 13:45:11 +0100

    Test different substrings

--- source/test/applications/strings/TestCalcDifferentSubstrings.cpp
+++ source/test/applications/strings/TestCalcDifferentSubstrings.cpp
@@ -1,14 +1,35 @@
 #include <gmock/gmock.h>
 
 #include <cstring>
+#include <optional>
 
 #include "./../../../applications/strings/calc_different_substrings/calc_different_substrings.h"
 
+namespace
+{
+void performTest(const std::string& str, std::optional<size_t> expected = std::nullopt)
+{
+  auto resultNaive = calcDifferentSubstringsNaive(str);
+  auto resultKasai = calcDifferentSubstringsKasai(str);
+  // auto resultSubstringsAutomaton1 = calcDifferentSubstringsAutomaton1(str);
+  auto resultSubstringsAutomaton2 = calcDifferentSubstringsAutomaton2(str);
+  if (!expected) {
+    expected = resultNaive;
+  }
+  EXPECT_EQ(resultNaive, *expected);
+  EXPECT_EQ(resultKasai, *expected);
+  // EXPECT_EQ(resultSubstringsAutomaton1, *expected);
+  EXPECT_EQ(resultSubstringsAutomaton2, *expected);
+}
+}  // namespace
+
 TEST(CalcDifferentSubstrings, TestCalcDifferentSubstrings)
 {
-  std::string str = "banana";
-  std::cout << calcDifferentSubstringsNaive(str) << "\n";
-  std::cout << calcDifferentSubstringsKasai(str) << "\n";
-  std::cout << calcDifferentSubstringsAutomaton1(str) << "\n";
-  std::cout << calcDifferentSubstringsAutomaton2(str) << "\n";
+  performTest("banana", 15);
+  performTest("", 0);
+  performTest("a", 1);
+  performTest("aa", 2);
+  performTest("ab", 3);
+  performTest("bbbaaabababbabbbaaaaabbbbbb");
+  performTest("jGjjHG85_???##??###_843");
 }
