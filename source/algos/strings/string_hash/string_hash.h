ee35af09b4d332272b8a5c928ba0351b84b5ea2f
Andrzej Polak <jjjkamu@gmail.com>
2024-02-15 17:22:15 +0100

    Add hash algos + test

--- source/algos/strings/string_hash/string_hash.h
+++ source/algos/strings/string_hash/string_hash.h
@@ -2,19 +2,38 @@
 #define ALGOS_STRINGS_STRING_HASH_INCLUDED
 
 #include <string>
+#include <vector>
 
 class StringHash
 {
 public:
-  static long long stringHash(std::string const& s, long long p = 263, long long m = 1e9 + 9)
+  static std::pair<uint64_t, std::pair<uint64_t, uint64_t>> stringHash(std::string const& s, uint64_t p = 263,
+                                                                       uint64_t m = 1e9 + 9)
   {
-    long long hashValue = 0;
-    long long pow = 1;
+    uint64_t hashValue = 0;
+    uint64_t pow = 1;
     for (char c : s) {
       hashValue = (hashValue + (static_cast<int>(c) + 1) * pow) % m;
       pow = (pow * p) % m;
     }
-    return hashValue;
+    return {hashValue, {p, m}};
+  }
+
+  static std::pair<std::vector<uint64_t>, std::pair<uint64_t, uint64_t>> prefixHashes(std::string const& s,
+                                                                                      uint64_t p = 263,
+                                                                                      uint64_t m = 1e9 + 9)
+  {
+    uint64_t pow = 1;
+    std::vector<uint64_t> ret;
+    ret.reserve(s.size() + 1);
+    ret.push_back(0);
+    uint64_t hashValue = 0;
+    for (char c : s) {
+      hashValue = (hashValue + (static_cast<int>(c) + 1) * pow) % m;
+      pow = (pow * p) % m;
+      ret.push_back(hashValue);
+    }
+    return {ret, {p, m}};
   }
 };
 
--- source/test/algos/strings/TestStringHash.cpp
+++ source/test/algos/strings/TestStringHash.cpp
@@ -2,4 +2,75 @@
 
 #include "./../../../algos/strings/string_hash/string_hash.h"
 
-TEST(StringHash, TestStringHash) { ; }
+namespace
+{
+void testExpected(const std::string& str, size_t expected)
+{
+  const auto& [hash, params] = StringHash::stringHash(str);
+  const auto& [p, m] = params;
+  EXPECT_EQ(hash, expected);
+  EXPECT_EQ(p, 263);
+  EXPECT_EQ(m, 1e9 + 9);
+}
+
+void testExpected(const std::string& str, size_t expected, uint64_t p, uint64_t m)
+{
+  const auto& [hash, params] = StringHash::stringHash(str, p, m);
+  const auto& [p1, m1] = params;
+  EXPECT_EQ(hash, expected);
+  EXPECT_EQ(p, p1);
+  EXPECT_EQ(m, m1);
+}
+
+void testPrefix(const std::string& str)
+{
+  std::vector<uint64_t> hashes;
+  for (size_t i = 0; i <= str.size(); ++i) {
+    auto curr = str.substr(0, i);
+    const auto& [hash, params] = StringHash::stringHash(curr);
+    hashes.push_back(hash);
+  }
+  auto prefixHashes = StringHash::prefixHashes(str);
+  EXPECT_EQ(hashes, prefixHashes.first);
+}
+
+void testPrefix(const std::string& str, uint64_t p, uint64_t m)
+{
+  std::vector<uint64_t> hashes;
+  for (size_t i = 0; i <= str.size(); ++i) {
+    auto curr = str.substr(0, i);
+    const auto& [hash, params] = StringHash::stringHash(curr, p, m);
+    hashes.push_back(hash);
+  }
+  auto prefixHashes = StringHash::prefixHashes(str, p, m);
+  EXPECT_EQ(hashes, prefixHashes.first);
+}
+}  // namespace
+
+TEST(StringHash, TestStringHash)
+{
+  testExpected("", 0);
+  testExpected("", 0, 7, 71);
+  testExpected(" ", 33);
+  testExpected(" ", 33, 7, 71);
+  testExpected("a", 98);
+  testExpected("a", 27, 7, 71);
+  testExpected("banana", 703622327);
+  testExpected("banana", 703622327, 263, 1e9 + 9);
+  testExpected("bbbaaabababbabbbaaaaabbbbbb_jGjjHG85_???##??###_843", 598108708);
+  testExpected("bbbaaabababbabbbaaaaabbbbbb_jGjjHG85_???##??###_843", 51, 7, 71);
+}
+
+TEST(StringHash, TestPrefixHashes)
+{
+  testPrefix("");
+  testPrefix("", 7, 71);
+  testPrefix(" ");
+  testPrefix(" ", 7, 71);
+  testPrefix("a");
+  testPrefix("a", 7, 71);
+  testPrefix("banana");
+  testPrefix("banana", 263, 1e9 + 9);
+  testPrefix("bbbaaabababbabbbaaaaabbbbbb_jGjjHG85_???##??###_843");
+  testPrefix("bbbaaabababbabbbaaaaabbbbbb_jGjjHG85_???##??###_843", 7, 71);
+}
