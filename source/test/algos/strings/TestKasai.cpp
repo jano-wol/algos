a1fdaa278e8f652c024dd7c002a4326a2e781ea2
Andrzej Polak <jjjkamu@gmail.com>
2024-02-15 11:07:21 +0100

    TestKasai

--- source/test/algos/strings/TestKasai.cpp
+++ source/test/algos/strings/TestKasai.cpp
@@ -10,13 +10,35 @@ TEST(Kasai, TestKasai)
   std::string str = "banana";
   auto result = Kasai::kasai(str);
   auto resultNaive = KasaiNaive::kasai(str);
+  std::vector<size_t> expectedP = {6, 5, 3, 1, 0, 4, 2};
+  std::vector<size_t> expectedLcp = {0, 1, 3, 0, 0, 2};
+  EXPECT_EQ(result.first, expectedP);
+  EXPECT_EQ(resultNaive.first, expectedP);
+  EXPECT_EQ(result.second, expectedLcp);
+  EXPECT_EQ(resultNaive.second, expectedLcp);
 
-  for (auto a : resultNaive.second) {
-    std::cout << a << " ";
-  }
-  std::cout << "\n";
-  for (auto a : result.second) {
-    std::cout << a << " ";
-  }
-  std::cout << "\n";
+  str = "";
+  result = Kasai::kasai(str);
+  resultNaive = KasaiNaive::kasai(str);
+  expectedP = {0};
+  expectedLcp = {};
+  EXPECT_EQ(result.first, expectedP);
+  EXPECT_EQ(resultNaive.first, expectedP);
+  EXPECT_EQ(result.second, expectedLcp);
+  EXPECT_EQ(resultNaive.second, expectedLcp);
+
+  str = "a";
+  result = Kasai::kasai(str);
+  resultNaive = KasaiNaive::kasai(str);
+  expectedP = {1, 0};
+  expectedLcp = {0};
+  EXPECT_EQ(result.first, expectedP);
+  EXPECT_EQ(resultNaive.first, expectedP);
+  EXPECT_EQ(result.second, expectedLcp);
+  EXPECT_EQ(resultNaive.second, expectedLcp);
+
+  str = "bbbaaabababbabbbaaaaabbbbbb";
+  result = Kasai::kasai(str);
+  resultNaive = KasaiNaive::kasai(str);
+  EXPECT_EQ(result, resultNaive);
 }
