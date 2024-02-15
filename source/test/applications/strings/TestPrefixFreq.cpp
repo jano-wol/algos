1499be009fc9ad525185e122142ca26f05e4bb8c
Andrzej Polak <jjjkamu@gmail.com>
2024-02-15 12:31:43 +0100

    Add prefix freq test

--- source/test/applications/strings/TestPrefixFreq.cpp
+++ source/test/applications/strings/TestPrefixFreq.cpp
@@ -4,19 +4,33 @@
 
 #include "./../../../applications/strings/prefix_freq/prefix_freq.h"
 
+namespace
+{
+void testExpected(const std::string& str, const std::vector<size_t>& expected)
+{
+  auto result = prefixFreq(str);
+  auto resultNaive = prefixFreqNaive(str);
+  EXPECT_EQ(result, expected);
+  EXPECT_EQ(resultNaive, expected);
+}
+
+void testAlignment(const std::string& str)
+{
+  auto result = prefixFreq(str);
+  auto resultNaive = prefixFreqNaive(str);
+  EXPECT_EQ(result, resultNaive);
+}
+}  // namespace
+
 TEST(PrefixFreq, TestPrefixFreq)
 {
-  std::string str = "aabaaabaaaba";
-
-  auto resultNaive = prefixFreqNaive(str);
-  auto result = prefixFreq(str);
-
-  for (auto a : resultNaive) {
-    std::cout << a << " ";
-  }
-  std::cout << "\n";
-  for (auto a : result) {
-    std::cout << a << " ";
-  }
-  std::cout << "\n";
+  testExpected("aabaaabaaaba", {9, 5, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1});
+  testExpected("banana", {1, 1, 1, 1, 1, 1});
+  testExpected("aaaa", {4, 3, 2, 1});
+  testExpected("aa", {2, 1});
+  testExpected("ab", {1, 1});
+  testExpected("a", {1});
+  testExpected("", {});
+  testAlignment(std::string("bbbaaabababbabbbaaaaabbbbbb"));
+  testAlignment(std::string("jGjjHG85_???##??###_843"));
 }
