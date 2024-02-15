a53193e234d3c77231e18c5e89f0074733067cf0
Andrzej Polak <jjjkamu@gmail.com>
2024-02-15 11:30:01 +0100

    Test prefix function

--- source/test/algos/strings/TestPrefixFunction.cpp
+++ source/test/algos/strings/TestPrefixFunction.cpp
@@ -10,13 +10,45 @@ TEST(PrefixFunction, TestPrefixFunction)
   std::string str = "aabaaab";
   auto pi = PrefixFunction::prefixFunction(str);
   auto piNaive = PrefixFunctionNaive::prefixFunction(str);
+  std::vector<size_t> expected = {0, 1, 0, 1, 2, 2, 3};
+  EXPECT_EQ(pi, expected);
+  EXPECT_EQ(piNaive, expected);
 
-  for (auto a : piNaive) {
-    std::cout << a << " ";
-  }
-  std::cout << "\n";
-  for (auto a : pi) {
-    std::cout << a << " ";
-  }
-  std::cout << "\n";
+  str = "";
+  pi = PrefixFunction::prefixFunction(str);
+  piNaive = PrefixFunctionNaive::prefixFunction(str);
+  expected = {};
+  EXPECT_EQ(pi, expected);
+  EXPECT_EQ(piNaive, expected);
+
+  str = "a";
+  pi = PrefixFunction::prefixFunction(str);
+  piNaive = PrefixFunctionNaive::prefixFunction(str);
+  expected = {0};
+  EXPECT_EQ(pi, expected);
+  EXPECT_EQ(piNaive, expected);
+
+  str = "aa";
+  pi = PrefixFunction::prefixFunction(str);
+  piNaive = PrefixFunctionNaive::prefixFunction(str);
+  expected = {0, 1};
+  EXPECT_EQ(pi, expected);
+  EXPECT_EQ(piNaive, expected);
+
+  str = "ab";
+  pi = PrefixFunction::prefixFunction(str);
+  piNaive = PrefixFunctionNaive::prefixFunction(str);
+  expected = {0, 0};
+  EXPECT_EQ(pi, expected);
+  EXPECT_EQ(piNaive, expected);
+
+  str = "bbbaaabababbabbbaaaaabbbbbb";
+  pi = PrefixFunction::prefixFunction(str);
+  piNaive = PrefixFunctionNaive::prefixFunction(str);
+  EXPECT_EQ(pi, piNaive);
+
+  str = "jGjjHG85_???##??###_843";
+  pi = PrefixFunction::prefixFunction(str);
+  piNaive = PrefixFunctionNaive::prefixFunction(str);
+  EXPECT_EQ(pi, piNaive);
 }
