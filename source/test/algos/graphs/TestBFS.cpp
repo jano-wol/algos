ae3ab06ece9683fd258535fb25e3c50444f393e5
Andrzej Polak <jjjkamu@gmail.com>
2024-02-20 16:47:14 +0100

    Finalize bfs tests

--- source/test/algos/graphs/TestBFS.cpp
+++ source/test/algos/graphs/TestBFS.cpp
@@ -1,6 +1,5 @@
 #include <gmock/gmock.h>
 
-#include <random>
 #include <vector>
 
 #include "./../../../algos/graphs/bfs/bfs.h"
@@ -8,36 +7,6 @@
 
 namespace
 {
-void testNumberOfComponents(size_t n, std::vector<std::pair<size_t, size_t>> edges, size_t expected)
-{
-  auto adj = edgesToAdj(n, edges);
-  BFS bfs(n, adj);
-  auto components = bfs.partitiate();
-  int max = -1;
-  for (auto c : components) {
-    if (int(c) > max) {
-      max = c;
-    }
-  }
-  int numberOfComponents = max + 1;
-
-  EXPECT_EQ(numberOfComponents, expected);
-}
-
-void testRandomGraph(size_t n, double p)
-{
-  std::vector<std::pair<size_t, size_t>> edges;
-  std::mt19937 e;
-  for (size_t i = 0; i < n; ++i) {
-    for (size_t j = 0; j < n; ++j) {
-      if (static_cast<double>(e() % 100) < p * 100.0) {
-        edges.push_back({i, j});
-      }
-    }
-  }
-  testNumberOfComponents(n, edges, numberOfComponentsNaive(n, edges));
-}
-
 void testPath(size_t n, std::vector<std::pair<size_t, size_t>> edges, size_t source, size_t target,
               std::vector<size_t> expected)
 {
@@ -47,34 +16,15 @@ void testPath(size_t n, std::vector<std::pair<size_t, size_t>> edges, size_t sou
   EXPECT_EQ(*(bfs.getPathFromSource(target, structure)), expected);
   EXPECT_EQ(*(bfs.getDistanceFromSource(target, structure)), expected.size() - 1);
 }
-
 }  // namespace
 
 TEST(BFS, TestBFS)
 {
-  testNumberOfComponents(0, {}, 0);
-  testNumberOfComponents(1, {}, 1);
-  testNumberOfComponents(1, {{0, 0}}, 1);
-  testNumberOfComponents(2, {{0, 0}, {1, 1}}, 2);
-  testNumberOfComponents(2, {}, 2);
-  testNumberOfComponents(2, {{0, 1}, {0, 1}}, 1);
-  testNumberOfComponents(3, {}, 3);
-  testNumberOfComponents(3, {{0, 1}}, 2);
-  testNumberOfComponents(3, {{0, 1}, {0, 1}}, 2);
-  testNumberOfComponents(3, {{0, 1}, {0, 2}}, 1);
-  testNumberOfComponents(3, {{0, 1}, {0, 2}, {1, 2}}, 1);
-  testNumberOfComponents(4, {}, 4);
-  testNumberOfComponents(4, {{1, 3}}, 3);
-  testNumberOfComponents(4, {{0, 1}, {2, 3}}, 2);
-  testNumberOfComponents(4, {{0, 3}, {2, 3}}, 2);
-  testNumberOfComponents(4, {{0, 3}, {2, 3}, {0, 2}}, 2);
-  testNumberOfComponents(4, {{0, 3}, {2, 3}, {1, 3}}, 1);
-  testRandomGraph(15, 0.0);
-  testRandomGraph(15, 0.01);
-  testRandomGraph(15, 0.05);
-  testRandomGraph(15, 0.1);
-  testRandomGraph(15, 0.15);
+  testPath(1, {}, 0, 0, {0});
   testPath(1, {{0, 0}}, 0, 0, {0});
+  testPath(2, {{0, 1}}, 0, 0, {0});
+  testPath(2, {{0, 1}}, 1, 0, {1, 0});
+  testPath(2, {{0, 1}, {0, 1}}, 1, 0, {1, 0});
   testPath(4, {{0, 1}, {2, 3}}, 2, 3, {2, 3});
   testPath(4, {{0, 1}, {2, 3}}, 1, 0, {1, 0});
   testPath(4, {{0, 1}, {2, 3}}, 2, 2, {2});
