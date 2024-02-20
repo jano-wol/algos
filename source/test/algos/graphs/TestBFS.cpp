4eff88ee83570e28fc14c3b3671ec820e6ea9739
Andrzej Polak <jjjkamu@gmail.com>
2024-02-20 15:15:09 +0100

    Add more tests

--- source/test/algos/graphs/TestBFS.cpp
+++ source/test/algos/graphs/TestBFS.cpp
@@ -80,6 +80,17 @@ void testRandomGraph(size_t n, double p)
   }
   testNumberOfComponents(n, edges, calcComponents(n, edges));
 }
+
+void testPath(size_t n, std::vector<std::pair<size_t, size_t>> edges, size_t source, size_t target,
+              std::vector<size_t> expected)
+{
+  auto adj = edgesToAdj(n, edges);
+  BFS bfs(n, adj);
+  auto structure = bfs.getComponent(source);
+  EXPECT_EQ(*(bfs.getPathFromSource(target, structure)), expected);
+  EXPECT_EQ(*(bfs.getDistanceFromSource(target, structure)), expected.size() - 1);
+}
+
 }  // namespace
 
 TEST(BFS, TestBFS)
@@ -106,4 +117,15 @@ TEST(BFS, TestBFS)
   testRandomGraph(15, 0.05);
   testRandomGraph(15, 0.1);
   testRandomGraph(15, 0.15);
+  testPath(4, {{0, 1}, {2, 3}}, 2, 3, {2, 3});
+  testPath(4, {{0, 1}, {2, 3}}, 1, 0, {1, 0});
+  testPath(4, {{0, 1}, {2, 3}}, 2, 2, {2});
+  testPath(4, {{0, 1}, {2, 3}}, 0, 0, {0});
+  testPath(5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}}, 1, 4, {1, 2, 3, 4});
+  testPath(5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}}, 1, 0, {1, 0});
+  testPath(5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}}, 1, 1, {1});
+  testPath(5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}}, 1, 0, {1, 0});
+  testPath(5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}}, 1, 4, {1, 0, 4});
+  testPath(5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}}, 1, 3, {1, 2, 3});
+  testPath(5, {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 0}}, 1, 2, {1, 2});
 }
