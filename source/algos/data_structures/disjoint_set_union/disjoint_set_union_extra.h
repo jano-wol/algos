111c7c9cf66cf0a690e8bae6858171e895cb85f3
Andrzej Polak <jjjkamu@gmail.com>
2024-02-21 15:03:31 +0100

    Add disjoint interval union

---
+++ source/algos/data_structures/disjoint_set_union/disjoint_set_union_extra.h
@@ -0,0 +1,69 @@
+#ifndef ALGOS_DATA_STRUCTURES_DISJOINT_SET_UNION_EXTRA_INCLUDED
+#define ALGOS_DATA_STRUCTURES_DISJOINT_SET_UNION_EXTRA_INCLUDED
+
+#include <functional>
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class DisjointSetUnionExtra
+{
+public:
+  // runtime = O(n), memory = O(n).
+  DisjointSetUnionExtra(size_t n_, std::vector<T> extra_,
+                        std::function<void(size_t, size_t, std::vector<T>&)> extraOp_)
+  {
+    n = n_;
+    extra = extra_;
+    extraOp = extraOp_;
+    numberOfComponents = n;
+    parent.resize(n);
+    size.resize(n, 1);
+    for (size_t i = 0; i < n; ++i) {
+      parent[i] = i;
+    }
+  }
+
+  // runtime = amortized O(alpha(n)), memory = O(1), where alpha(n) is the inverse Ackermann function
+  size_t findSet(size_t v)
+  {
+    if (v >= n) {
+      throw std::overflow_error("v is out of bound");
+    }
+    if (v == parent[v]) {
+      return v;
+    }
+    return parent[v] = findSet(parent[v]);
+  }
+
+  // runtime = amortized O(alpha(n)), memory = O(1), where alpha(n) is the inverse Ackermann function
+  void unionSets(size_t a, size_t b)
+  {
+    a = findSet(a);
+    b = findSet(b);
+    if (a != b) {
+      if (size[a] < size[b])
+        std::swap(a, b);
+      parent[b] = a;
+      size[a] += size[b];
+      extraOp(a, b, extra);
+      --numberOfComponents;
+    }
+  }
+
+  size_t getN() const { return n; }
+  size_t getNumberOfComponents() const { return numberOfComponents; }
+  const std::vector<size_t>& getParent() const { return parent; }
+  const std::vector<size_t>& getSize() const { return size; }
+  const std::vector<T>& getExtra() const { return extra; }
+
+private:
+  size_t n;
+  size_t numberOfComponents;
+  std::vector<size_t> parent;
+  std::vector<size_t> size;
+  std::vector<T> extra;
+  std::function<void(size_t, size_t, std::vector<T>&)> extraOp;
+};
+
+#endif  // ALGOS_DATA_STRUCTURES_DISJOINT_SET_UNION_INCLUDED
--- source/test/algos/data_structures/TestDisjointSetUnion.cpp
+++ source/test/algos/data_structures/TestDisjointSetUnion.cpp
@@ -3,10 +3,12 @@
 #include <vector>
 
 #include "./../../../algos/data_structures/disjoint_set_union/disjoint_set_union.h"
+#include "./../../../algos/data_structures/disjoint_set_union/disjoint_set_union_extra.h"
 
 namespace
 {
-void testSizes(size_t n, std::vector<std::pair<size_t, size_t>> connections, std::vector<size_t> expected)
+void testSizesDisjointSetUnion(size_t n, std::vector<std::pair<size_t, size_t>> connections,
+                               std::vector<size_t> expected)
 {
   DisjointSetUnion d(n);
   for (const auto& [a, b] : connections) {
@@ -19,25 +21,70 @@ void testSizes(size_t n, std::vector<std::pair<size_t, size_t>> connections, std
   }
   EXPECT_EQ(sizes, expected);
 }
+
+template <typename T>
+void testSizesDisjointSetUnionExtra(size_t n, std::vector<std::pair<size_t, size_t>> connections,
+                                    std::vector<size_t> expected)
+{
+  std::vector<T> s(n, 1);
+  auto extraOp = [](size_t a, size_t b, std::vector<T>& extra) -> void { extra[a] += extra[b]; };
+  DisjointSetUnionExtra<T> d(n, s, extraOp);
+  for (const auto& [a, b] : connections) {
+    d.unionSets(a, b);
+  }
+  auto size = d.getSize();
+  std::vector<size_t> sizes1;
+  for (size_t i = 0; i < n; ++i) {
+    sizes1.push_back(size[d.findSet(i)]);
+  }
+  auto extra = d.getExtra();
+  std::vector<size_t> sizes2;
+  for (size_t i = 0; i < n; ++i) {
+    sizes2.push_back(extra[d.findSet(i)]);
+  }
+  EXPECT_EQ(sizes1, expected);
+  EXPECT_EQ(sizes2, expected);
+}
 }  // namespace
 
 TEST(DisjointSetUnion, TestDisjointSetUnion)
 {
-  testSizes(0, {}, {});
-  testSizes(1, {}, {1});
-  testSizes(1, {{0, 0}}, {1});
-  testSizes(2, {{0, 0}, {1, 1}}, {1, 1});
-  testSizes(2, {}, {1, 1});
-  testSizes(2, {{0, 1}, {0, 1}}, {2, 2});
-  testSizes(3, {}, {1, 1, 1});
-  testSizes(3, {{0, 1}}, {2, 2, 1});
-  testSizes(3, {{0, 1}, {0, 1}}, {2, 2, 1});
-  testSizes(3, {{0, 1}, {0, 2}}, {3, 3, 3});
-  testSizes(3, {{0, 1}, {0, 2}, {1, 2}}, {3, 3, 3});
-  testSizes(4, {}, {1, 1, 1, 1});
-  testSizes(4, {{1, 3}}, {1, 2, 1, 2});
-  testSizes(4, {{0, 1}, {2, 3}}, {2, 2, 2, 2});
-  testSizes(4, {{0, 3}, {2, 3}}, {3, 1, 3, 3});
-  testSizes(4, {{0, 3}, {2, 3}, {0, 2}}, {3, 1, 3, 3});
-  testSizes(4, {{0, 3}, {2, 3}, {1, 3}}, {4, 4, 4, 4});
+  testSizesDisjointSetUnion(0, {}, {});
+  testSizesDisjointSetUnion(1, {}, {1});
+  testSizesDisjointSetUnion(1, {{0, 0}}, {1});
+  testSizesDisjointSetUnion(2, {{0, 0}, {1, 1}}, {1, 1});
+  testSizesDisjointSetUnion(2, {}, {1, 1});
+  testSizesDisjointSetUnion(2, {{0, 1}, {0, 1}}, {2, 2});
+  testSizesDisjointSetUnion(3, {}, {1, 1, 1});
+  testSizesDisjointSetUnion(3, {{0, 1}}, {2, 2, 1});
+  testSizesDisjointSetUnion(3, {{0, 1}, {0, 1}}, {2, 2, 1});
+  testSizesDisjointSetUnion(3, {{0, 1}, {0, 2}}, {3, 3, 3});
+  testSizesDisjointSetUnion(3, {{0, 1}, {0, 2}, {1, 2}}, {3, 3, 3});
+  testSizesDisjointSetUnion(4, {}, {1, 1, 1, 1});
+  testSizesDisjointSetUnion(4, {{1, 3}}, {1, 2, 1, 2});
+  testSizesDisjointSetUnion(4, {{0, 1}, {2, 3}}, {2, 2, 2, 2});
+  testSizesDisjointSetUnion(4, {{0, 3}, {2, 3}}, {3, 1, 3, 3});
+  testSizesDisjointSetUnion(4, {{0, 3}, {2, 3}, {0, 2}}, {3, 1, 3, 3});
+  testSizesDisjointSetUnion(4, {{0, 3}, {2, 3}, {1, 3}}, {4, 4, 4, 4});
+}
+
+TEST(DisjointSetUnionExtra, TestDisjointSetUnionExtra)
+{
+  testSizesDisjointSetUnionExtra<int>(0, {}, {});
+  testSizesDisjointSetUnionExtra<int>(1, {}, {1});
+  testSizesDisjointSetUnionExtra<int>(1, {{0, 0}}, {1});
+  testSizesDisjointSetUnionExtra<int>(2, {{0, 0}, {1, 1}}, {1, 1});
+  testSizesDisjointSetUnionExtra<int>(2, {}, {1, 1});
+  testSizesDisjointSetUnionExtra<int>(2, {{0, 1}, {0, 1}}, {2, 2});
+  testSizesDisjointSetUnionExtra<int>(3, {}, {1, 1, 1});
+  testSizesDisjointSetUnionExtra<int>(3, {{0, 1}}, {2, 2, 1});
+  testSizesDisjointSetUnionExtra<int>(3, {{0, 1}, {0, 1}}, {2, 2, 1});
+  testSizesDisjointSetUnionExtra<int>(3, {{0, 1}, {0, 2}}, {3, 3, 3});
+  testSizesDisjointSetUnionExtra<int>(3, {{0, 1}, {0, 2}, {1, 2}}, {3, 3, 3});
+  testSizesDisjointSetUnionExtra<int>(4, {}, {1, 1, 1, 1});
+  testSizesDisjointSetUnionExtra<int>(4, {{1, 3}}, {1, 2, 1, 2});
+  testSizesDisjointSetUnionExtra<int>(4, {{0, 1}, {2, 3}}, {2, 2, 2, 2});
+  testSizesDisjointSetUnionExtra<int>(4, {{0, 3}, {2, 3}}, {3, 1, 3, 3});
+  testSizesDisjointSetUnionExtra<int>(4, {{0, 3}, {2, 3}, {0, 2}}, {3, 1, 3, 3});
+  testSizesDisjointSetUnionExtra<int>(4, {{0, 3}, {2, 3}, {1, 3}}, {4, 4, 4, 4});
 }
