9420a260b9f0d32b75132082109b357f54a421dd
Andrzej Polak <jjjkamu@gmail.com>
2024-02-22 13:04:57 +0100

    Start testing lca

---
+++ source/test/applications/graphs/TestLeastCommonAncestor.cpp
@@ -0,0 +1,20 @@
+#include <gmock/gmock.h>
+
+#include <random>
+
+#include "./../../../applications/graphs/least_common_ancestor/least_common_ancestor.h"
+
+namespace
+{
+void testLeastCommonAncestor(size_t n, std::vector<std::pair<size_t, size_t>> edges,
+                             std::vector<std::pair<size_t, size_t>> querys, std::vector<size_t> expected)
+{
+  auto resultNaive = leastCommonAncestorNaive(n, edges, querys);
+  EXPECT_EQ(resultNaive, expected);
+}
+}  // namespace
+
+TEST(LeastCommonAncestor, TestLeastCommonAncestor)
+{
+  testLeastCommonAncestor(0, {}, {}, {});
+}
