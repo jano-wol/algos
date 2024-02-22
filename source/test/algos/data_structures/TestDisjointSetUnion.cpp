33aed8426b5ce25c6d23702baaa7a5d1d20d7754
Andrzej Polak <jjjkamu@gmail.com>
2024-02-22 10:28:15 +0100

    Add tests

--- source/test/algos/data_structures/TestDisjointSetUnion.cpp
+++ source/test/algos/data_structures/TestDisjointSetUnion.cpp
@@ -103,4 +103,18 @@ TEST(DisjointSetUnionCompress, TestDisjointSetUnionCompress)
   DisjointSetUnionCompress d(0);
   testSizesDisjointSetUnionCompress(1, {}, 0, 0);
   testSizesDisjointSetUnionCompress(1, {{0, 0}}, 0, 0);
+  testSizesDisjointSetUnionCompress(1, {{0, 0}, {0, 0}}, 0, 0);
+  testSizesDisjointSetUnionCompress(2, {}, 0, 0);
+  testSizesDisjointSetUnionCompress(2, {}, 1, 1);
+  testSizesDisjointSetUnionCompress(2, {{0, 1}, {1, 1}}, 0, 1);
+  testSizesDisjointSetUnionCompress(2, {{0, 1}, {1, 1}}, 1, 1);
+  testSizesDisjointSetUnionCompress(2, {{0, 1}}, 0, 1);
+  testSizesDisjointSetUnionCompress(2, {{0, 1}, {0, 1}}, 1, 1);
+  testSizesDisjointSetUnionCompress(7, {{0, 2}, {1, 2}, {2, 3}, {4, 3}, {5, 4}, {6, 4}}, 0, 3);
+  testSizesDisjointSetUnionCompress(7, {{0, 2}, {1, 2}, {2, 3}, {4, 3}, {5, 4}, {6, 4}}, 1, 3);
+  testSizesDisjointSetUnionCompress(7, {{0, 2}, {1, 2}, {2, 3}, {4, 3}, {5, 4}, {6, 4}}, 2, 3);
+  testSizesDisjointSetUnionCompress(7, {{0, 2}, {1, 2}, {2, 3}, {4, 3}, {5, 4}, {6, 4}}, 3, 3);
+  testSizesDisjointSetUnionCompress(7, {{0, 2}, {1, 2}, {2, 3}, {4, 3}, {5, 4}, {6, 4}}, 5, 3);
+  testSizesDisjointSetUnionCompress(7, {{0, 2}, {1, 2}, {2, 3}, {4, 3}, {5, 4}, {6, 4}}, 6, 3);
+  testSizesDisjointSetUnionCompress(9, {{0, 2}, {1, 2}, {2, 3}, {4, 3}, {5, 4}, {6, 4}, {7, 8}}, 7, 8);
 }
