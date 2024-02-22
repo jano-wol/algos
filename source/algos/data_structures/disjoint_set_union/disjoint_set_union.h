f6b941129733a9d68d8c7094add6ee1d3649aefc
Andrzej Polak <jjjkamu@gmail.com>
2024-02-22 09:55:12 +0100

    Use rank

--- source/algos/data_structures/disjoint_set_union/disjoint_set_union.h
+++ source/algos/data_structures/disjoint_set_union/disjoint_set_union.h
@@ -10,12 +10,13 @@ public:
   // runtime = O(n), memory = O(n).
   DisjointSetUnion(size_t n_) : n(n_)
   {
-    numberOfComponents = n;
     parent.resize(n);
-    size.resize(n, 1);
     for (size_t i = 0; i < n; ++i) {
       parent[i] = i;
     }
+    rank.resize(n, 0);
+    numberOfComponents = n;
+    size.resize(n, 1);
   }
 
   // runtime = amortized O(alpha(n)), memory = O(1), where alpha(n) is the inverse Ackermann function.
@@ -36,9 +37,13 @@ public:
     a = findSet(a);
     b = findSet(b);
     if (a != b) {
-      if (size[a] < size[b])
+      if (rank[a] < rank[b]) {
         std::swap(a, b);
+      }
       parent[b] = a;
+      if (rank[a] == rank[b]) {
+        ++rank[a];
+      }
       size[a] += size[b];
       --numberOfComponents;
     }
@@ -51,9 +56,10 @@ public:
   const std::vector<size_t>& getSize() const { return size; }
 
 private:
+  std::vector<size_t> parent;
+  std::vector<size_t> rank;
   size_t n;
   size_t numberOfComponents;
-  std::vector<size_t> parent;
   std::vector<size_t> size;
 };
 
--- source/algos/data_structures/disjoint_set_union/disjoint_set_union_extra.h
+++ source/algos/data_structures/disjoint_set_union/disjoint_set_union_extra.h
@@ -13,12 +13,13 @@ public:
   DisjointSetUnionExtra(size_t n_, std::vector<T> extra_, std::function<void(size_t, size_t, std::vector<T>&)> extraOp_)
       : n(n_), extra(std::move(extra_)), extraOp(std::move(extraOp_))
   {
-    numberOfComponents = n;
     parent.resize(n);
-    size.resize(n, 1);
     for (size_t i = 0; i < n; ++i) {
       parent[i] = i;
     }
+    rank.resize(n, 0);
+    numberOfComponents = n;
+    size.resize(n, 1);
   }
 
   // runtime = amortized O(alpha(n)), memory = O(1), where alpha(n) is the inverse Ackermann function.
@@ -39,9 +40,13 @@ public:
     a = findSet(a);
     b = findSet(b);
     if (a != b) {
-      if (size[a] < size[b])
+      if (rank[a] < rank[b]) {
         std::swap(a, b);
+      }
       parent[b] = a;
+      if (rank[a] == rank[b]) {
+        ++rank[a];
+      }
       size[a] += size[b];
       extraOp(a, b, extra);
       --numberOfComponents;
@@ -57,9 +62,10 @@ public:
   std::vector<T>& getExtraMutable() { return extra; }
 
 private:
+  std::vector<size_t> parent;
+  std::vector<size_t> rank;
   size_t n;
   size_t numberOfComponents;
-  std::vector<size_t> parent;
   std::vector<size_t> size;
   std::vector<T> extra;
   std::function<void(size_t, size_t, std::vector<T>&)> extraOp;
