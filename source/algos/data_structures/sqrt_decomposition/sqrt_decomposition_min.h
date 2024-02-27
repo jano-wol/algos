18969a442d8def83914abce48c536b9fefcf1162
Andrzej Polak <jjjkamu@gmail.com>
2024-02-27 22:01:11 +0100

    try tio implement min

--- source/algos/data_structures/sqrt_decomposition/sqrt_decomposition_min.h
+++ source/algos/data_structures/sqrt_decomposition/sqrt_decomposition_min.h
@@ -1,73 +1,105 @@
-#ifndef ALGOS_DATA_STRUCTURE_SQRT_DECOMPOSITION_SUM_INCLUDED
-#define ALGOS_DATA_STRUCTURE_SQRT_DECOMPOSITION_SUM_INCLUDED
+#ifndef ALGOS_DATA_STRUCTURE_SQRT_DECOMPOSITION_MIN_INCLUDED
+#define ALGOS_DATA_STRUCTURE_SQRT_DECOMPOSITION_MIN_INCLUDED
 
 #include <vector>
 
 template <typename T>
-class SqrtDecompositionSum
+class SqrtDecompositionMin
 {
 public:
   // runtime = O(n), memory = O(n), where n = |a|.
-  SqrtDecompositionSum(std::vector<T> a_)
-      : n(a_.size()), len(size_t(sqrt(n + .0)) + 1), a(std::move(a_)), blockSums(len), blockFixes(len)
+  SqrtDecompositionMin(std::vector<T> a_)
+      : n(a_.size()), len(size_t(sqrt(n + .0)) + 1), a(std::move(a_)), blockMins(len), blockMonotone(len, false)
   {
-    for (size_t i = 0; i < n; ++i) {
-      blockSums[i / len] += a[i];
+    size_t i = 0;
+    size_t j = 0;
+    for (; i < (n / len); ++i) {
+      blockMins[i] = a[i * len];
+      j = i * len + 1;
+      for (; j < (i + 1) * len; ++j) {
+        blockMins[i] = std::min(blockMins[i], a[j]);
+      }
+    }
+    if (j + 1 < n) {
+      blockMins[i] = a[j];
+      ++j;
+      for (; j < n; ++j) {
+        blockMins[i] = std::min(blockMins[i], a[j]);
+      }
     }
   }
 
-  SqrtDecompositionSum(size_t n_) : SqrtDecompositionSum(std::vector<T>(n_)) {}
+  SqrtDecompositionMin(size_t n_) : SqrtDecompositionMin(std::vector<T>(n_)) {}
 
   // runtime = O(sqrt(n)), memory = O(1).
-  T sum(size_t l, size_t r) const
+  T min(size_t l, size_t r) const
   {
     rangeCheck(l, r);
-    T sum = 0;
+    T minVal = 0;
     size_t c_l = l / len;
     size_t c_r = r / len;
-    if (c_l == c_r)
-      for (size_t i = l; i <= r; ++i) sum += (a[i] + blockFixes[i / len]);
-    else {
-      for (size_t i = l, end = (c_l + 1) * len - 1; i <= end; ++i) sum += (a[i] + blockFixes[i / len]);
-      for (size_t i = c_l + 1; i < c_r; ++i) sum += (blockSums[i]);
-      for (size_t i = c_r * len; i <= r; ++i) sum += (a[i] + blockFixes[i / len]);
+    if (c_l == c_r) {
+      if (blockMonotone[c_l]) {
+        minVal = blockValue[c_l];
+      } else {
+        minVal = a[l];
+        for (size_t i = l + 1; i <= r; ++i) minVal = std::min(minVal, a[i]);
+      }
+    } else {
+      if (blockMonotone[c_l]) {
+        minVal = blockValue[c_l];
+      } else {
+        minVal = a[l];
+        for (size_t i = l + 1, end = (c_l + 1) * len - 1; i <= end; ++i) minVal = std::min(minVal, a[i]);
+      }
+      for (size_t i = c_l + 1; i < c_r; ++i) {
+        minVal = std::min(blockMins[i], minVal);
+      }
+      if (blockMonotone[c_r]) {
+        minVal = std::min(blockValue[c_r], minVal);
+      } else {
+        for (size_t i = c_r * len; i <= r; ++i) {
+          minVal = std::min(minVal, a[i]);
+        }
+      }
     }
-    return sum;
+    return minVal;
   }
 
   // runtime = O(sqrt(n)), memory = O(1).
   void increase(size_t l, size_t r, T val)
   {
-    rangeCheck(l, r);
-    size_t c_l = l / len;
-    size_t c_r = r / len;
-    if (c_l == c_r)
-      for (size_t i = l; i <= r; ++i) {
-        a[i] += val;
-        blockSums[i / len] += val;
-      }
-    else {
-      for (size_t i = l, end = (c_l + 1) * len - 1; i <= end; ++i) {
-        a[i] += val;
-        blockSums[i / len] += val;
-      }
-      for (size_t i = c_l + 1; i < c_r; ++i) {
-        blockSums[i] += T(val * len);
-        blockFixes[i] += val;
-      }
-      for (size_t i = c_r * len; i <= r; ++i) {
-        a[i] += val;
-        blockSums[i / len] += val;
-      }
-    }
+    /*     rangeCheck(l, r);
+        size_t c_l = l / len;
+        size_t c_r = r / len;
+        if (c_l == c_r)
+          for (size_t i = l; i <= r; ++i) {
+            a[i] += val;
+            blockSums[i / len] += val;
+          }
+        else {
+          for (size_t i = l, end = (c_l + 1) * len - 1; i <= end; ++i) {
+            a[i] += val;
+            blockSums[i / len] += val;
+          }
+          for (size_t i = c_l + 1; i < c_r; ++i) {
+            blockSums[i] += T(val * len);
+            blockFixes[i] += val;
+          }
+          for (size_t i = c_r * len; i <= r; ++i) {
+            a[i] += val;
+            blockSums[i / len] += val;
+          }
+        } */
   }
 
 private:
   size_t n;
   size_t len;
   std::vector<T> a;
-  std::vector<T> blockSums;
-  std::vector<T> blockFixes;
+  std::vector<T> blockMins;
+  std::vector<T> blockValue;
+  std::vector<bool> blockMonotone;
 
   void rangeCheck(size_t l, size_t r) const
   {
@@ -80,4 +112,4 @@ private:
   }
 };
 
-#endif  // ALGOS_DATA_STRUCTURE_SQRT_DECOMPOSITION_SUM_INCLUDED
+#endif  // ALGOS_DATA_STRUCTURE_SQRT_DECOMPOSITION_MIN_INCLUDED
