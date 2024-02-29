b0389b9b1eeff1824aecf8015e8ac9fce35faa33
Andrzej Polak <jjjkamu@gmail.com>
2024-02-29 12:15:19 +0100

    more tests

--- source/test/applications/data_structures/range_mode_queries/TestRangeModeQueries.cpp
+++ source/test/applications/data_structures/range_mode_queries/TestRangeModeQueries.cpp
@@ -21,9 +21,14 @@ TEST(RangeModeQuery, TestRangeModeQuery)
   testExpected(std::vector<int>{0}, {}, {});
   testExpected(std::vector<int>{0}, {{0, 0}}, {{0}});
   testExpected(std::vector<int>{0}, {{0, 0}, {0, 0}}, {{0}, {0}});
-  testExpected(std::vector<int>{7, 6, 7, 6, 7, 6, 7}, {{0, 2}, {1, 3}, {2, 4}, {3, 5}, {4, 6}}, {{7}, {6}, {7}, {6}, {7}});
-/*   testExpected(std::vector<int>{7, 6, 7, 6, 7, 6, 7}, {{0, 0}, {1, 1}, {2, 2}, {3, 3}, {1, 5}}, {7, 6, 7, 6, 6});
-  testExpected(std::vector<size_t>{7, 6, 7, 6, 7, 6, 7}, {{0, 6}}, {7});
+  testExpected(std::vector<int>{7, 6, 7, 6, 7, 6, 7}, {{0, 2}, {1, 3}, {2, 4}, {3, 5}, {4, 6}},
+               {{7}, {6}, {7}, {6}, {7}});
+  testExpected(std::vector<int>{7, 6, 7, 6, 7, 6, 7}, {{0, 3}, {1, 4}, {2, 5}, {3, 6}},
+               {{6, 7}, {6, 7}, {6, 7}, {6, 7}});
+  testExpected(std::vector<int>{7, 6, 7, 6, 7, 6, 7}, {{0, 6}, {1, 6}, {0, 5}, {1, 5}}, {{7}, {6, 7}, {6, 7}, {6}});
+  testExpected(std::vector<int>{7, 6, 7, 6, 7, 6, 7}, {{0, 0}, {1, 1}, {2, 2}, {3, 3}, {1, 5}}, {{7}, {6}, {7}, {6}, {6}});
+  testExpected(std::vector<size_t>{7, 6, 7, 6, 7, 6, 7}, {{0, 6}}, {{7}});
+  /*
   testExpected(std::vector<size_t>{7, 6, 7, 6, 7, 6, 7, 8, 8, 8, 8, 8},
                {{0, 6}, {0, 7}, {0, 8}, {0, 9}, {0, 10}, {0, 11}}, {7, 7, 7, 7, 7, 8});
   testExpected(std::vector<size_t>{8, 8, 8, 8, 8, 7, 6, 7, 6, 7, 6, 7},
