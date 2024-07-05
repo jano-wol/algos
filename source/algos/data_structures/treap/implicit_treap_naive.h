1ba8747d545660404f3e654e8de3c88078af8999
janowol <jjjkamu@gmail.com>
2024-07-05 07:23:06 +0200

    Add ImplicitTreapNaive

---
+++ source/algos/data_structures/treap/implicit_treap_naive.h
@@ -0,0 +1,47 @@
+#ifndef ALGOS_DATA_STRUCTURES_IMPLICIT_TREAP_NAIVE_INCLUDED
+#define ALGOS_DATA_STRUCTURES_IMPLICIT_TREAP_NAIVE_INCLUDED
+
+#include <algorithm>
+#include <vector>
+
+template <typename T>
+class ImplicitTreapNaive
+{
+public:
+  ImplicitTreapNaive() {}
+
+  ImplicitTreapNaive(size_t n) : data(n) {}
+
+  size_t size() { return data.size(); }
+
+  void push_back(T val) { data.push_back(val); }
+
+  void insert(T val, size_t pos)
+  {
+    if (pos >= size()) {
+      throw std::overflow_error("insert pos is out of bound");
+    }
+    data.insert(data.begin() + pos, val);
+  }
+
+  void erase(size_t pos)
+  {
+    if (pos >= size()) {
+      throw std::overflow_error("erase pos is out of bound");
+    }
+    data.erase(data.begin() + pos);
+  }
+
+  T& operator[](size_t pos)
+  {
+    if (pos >= size()) {
+      throw std::overflow_error("operator[] pos is out of bound");
+    }
+    return data[pos];
+  }
+
+private:
+  std::vector<T> data;
+};
+
+#endif  // ALGOS_DATA_STRUCTURES_IMPLICIT_TREAP_INCLUDED
