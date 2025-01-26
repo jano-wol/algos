3761258413555f57141d4e94fd018007be4e7992
janowol <jjjkamu@gmail.com>
2025-01-27 00:30:25 +0100

    Try to test

--- source/test/algos/data_structures/link_cut_tree/TestLinkCutTree.cpp
+++ source/test/algos/data_structures/link_cut_tree/TestLinkCutTree.cpp
@@ -32,45 +32,34 @@ void testLinkCutTree(int n, std::vector<std::pair<int, std::pair<int, int>>> com
     }
   }
 }
-/*
-template <typename T>
-void testFenwick(size_t n, std::vector<std::pair<std::pair<size_t, std::pair<size_t, size_t>>, T>> commands,
-                 std::vector<T> expected)
-{
-  testFenwick(std::vector<T>(n), commands, expected);
-}
 
-template <typename T>
-void testRandomCommands(std::vector<T> init, size_t steps)
+void testRandomCommands(int n, int steps)
 {
-  size_t n = init.size();
   std::mt19937 e;
-  Fenwick<T> f(init);
-  FenwickNaive<T> fNaive(init);
-  for (size_t idx = 0; idx < steps; ++idx) {
-    size_t type = e() % 2;
-    size_t l = e() % n;
-    size_t r = e() % n;
-    if (r < l) {
-      std::swap(l, r);
+  LinkCutTree tree(n);
+  LinkCutTreeNaive treeNaive(n);
+  std::vector<std::unordered_set<int>> adj(n);
+  for (int idx = 0; idx < steps; ++idx) {
+    int u = e() % n;
+    int v = e() % n;
+    bool res = tree.connected(u, v);
+    bool resNaive = tree.connected(u, v);
+    EXPECT_EQ(res, resNaive);
+    if (adj[u].count(v) == 0 && res == false) {
+      tree.link(u, v);
+      treeNaive.link(u, v);
+      adj[u].insert(v);
+      adj[v].insert(u);
+      continue;
     }
-    T val = e() % 100;
-    if (type == 0) {
-      T result = f.sum(l, r);
-      T resultNaive = fNaive.sum(l, r);
-      EXPECT_EQ(result, resultNaive);
-    } else {
-      f.increase(l, r, val);
-      fNaive.increase(l, r, val);
+    if (adj[u].count(v) == 1) {
+      tree.cut(u, v);
+      treeNaive.cut(u, v);
+      adj[u].erase(v);
+      adj[v].erase(u);
     }
   }
 }
-
-template <typename T>
-void testRandomCommands(size_t n, size_t steps)
-{
-  testRandomCommands(std::vector<T>(n), steps);
-} */
 }  // namespace
 
 TEST(LinkCutTree, TestLinkCutTree)
@@ -89,35 +78,23 @@ TEST(LinkCutTree, TestLinkCutTree)
   testLinkCutTree(
       2, {{2, {0, 1}}, {0, {1, 0}}, {2, {0, 1}}, {1, {0, 1}}, {2, {0, 1}}, {2, {0, 1}}, {0, {0, 1}}, {2, {1, 0}}},
       {false, true, false, false, true});
-  /*  testFenwick<int>(0, {}, {});
-    testFenwick<int>(std::vector<int>(), {}, {});
-    testFenwick<int>(1, {}, {});
-    testFenwick<int>(1, {{{0, {0, 0}}, 0}}, {0});
-    testFenwick<int>(1, {{{0, {0, 0}}, 0}, {{0, {0, 0}}, 0}, {{1, {0, 0}}, 5}, {{0, {0, 0}}, 0}}, {0, 0, 5});
-    testFenwick<int>(5, {{{1, {1, 3}}, 3}, {{1, {0, 2}}, 5}, {{1, {0, 0}}, -5}, {{0, {0, 4}}, 0}}, {19});
-    testFenwick<size_t>(std::vector<size_t>{4, 3, 2, 2, 1},
-                        {{{1, {1, 3}}, 3}, {{1, {0, 2}}, 5}, {{1, {0, 0}}, 1}, {{0, {0, 4}}, 0}}, {37});
-    testFenwick<size_t>(std::vector<size_t>{4, 3, 2, 2, 1},
-                        {{{1, {1, 3}}, 3}, {{1, {0, 2}}, 5}, {{1, {0, 0}}, 1}, {{0, {0, 0}}, 0}, {{0, {3, 4}}, 0}},
-                        {10, 6});
-    testFenwick<size_t>(std::vector<size_t>{4, 3, 2, 2, 1},
-                        {{{1, {1, 3}}, 3}, {{1, {0, 2}}, 5}, {{1, {4, 4}}, 1}, {{0, {0, 0}}, 0}, {{0, {3, 4}}, 0}},
-                        {9, 7});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {0, 0}}, 0}}, {6});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {1, 1}}, 0}}, {5});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {2, 2}}, 0}}, {4});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {3, 3}}, 0}}, {3});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {4, 4}}, 0}}, {2});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {5, 5}}, 0}}, {1});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {0, 5}}, 0}}, {21});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {0, 3}}, 0}}, {18});
-    testFenwick<size_t>(std::vector<size_t>{6, 5, 4, 3, 2, 1}, {{{0, {3, 5}}, 0}}, {6});
+  testLinkCutTree(4,
+                  {{2, {0, 1}},
+                   {0, {1, 0}},
+                   {2, {0, 1}},
+                   {0, {2, 3}},
+                   {2, {0, 2}},
+                   {2, {0, 3}},
+                   {2, {2, 3}},
+                   {0, {3, 1}},
+                   {2, {3, 0}},
+                   {2, {2, 0}},
+                   {1, {2, 3}},
+                   {2, {2, 0}},
+                   {2, {1, 3}}},
+                  {false, true, false, false, true, true, true, false, true});
 
-    for (size_t n = 2; n < 20; ++n) {
-      std::vector<int> v(n);
-      std::iota(std::begin(v), std::end(v), 1);
-      testRandomCommands<int>(n, 100);
-      testRandomCommands<int>(v, 100);
-    }
-    */
+  for (int n = 2; n < 20; ++n) {
+    testRandomCommands(n, 2000);
+  }
 }
